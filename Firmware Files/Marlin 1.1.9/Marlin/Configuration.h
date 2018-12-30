486ed80765496b25b6af72d48e4b4f4b54f425f3
Roy Berntsen <roy@r3d.no>
2018-12-30 19:46:28 +0100

    Update Configuration.h
    enabled autotuen PID menu on LCD

--- Firmware Files/Marlin 1.1.9/Marlin/Configuration.h
+++ Firmware Files/Marlin 1.1.9/Marlin/Configuration.h
@@ -368,7 +368,7 @@
 #define PID_MAX BANG_MAX // Limits current to nozzle while PID is active (see PID_FUNCTIONAL_RANGE below); 255=full current
 #define PID_K1 0.95      // Smoothing factor within any PID loop
 #if ENABLED(PIDTEMP)
-  //#define PID_AUTOTUNE_MENU // Add PID Autotune to the LCD "Temperature" menu to run M303 and apply the result.
+  #define PID_AUTOTUNE_MENU // Add PID Autotune to the LCD "Temperature" menu to run M303 and apply the result.
   //#define PID_DEBUG // Sends debug data to the serial port.
   //#define PID_OPENLOOP 1 // Puts PID in open loop. M104/M140 sets the output power from 0 to PID_MAX
   //#define SLOW_PWM_HEATERS // PWM with very low frequency (roughly 0.125Hz=8s) and minimum state time of approximately 1s useful for heaters driven by a relay
@@ -379,10 +379,10 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
-  // V-King With e3d V6
+  // V-King With e3d V6 - Do tune your own
   #define  DEFAULT_Kp 15
   #define  DEFAULT_Ki 1.5
-  #define  DEFAULT_Kd 40
+  #define  DEFAULT_Kd 50
 
 #endif // PIDTEMP
 
