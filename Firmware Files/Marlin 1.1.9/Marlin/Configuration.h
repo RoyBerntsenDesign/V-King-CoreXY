208be5054e5ac51840b23285b286e88522f76bd0
Roy Berntsen <roy@r3d.no>
2019-02-08 23:35:26 +0100

    uploaded cable buddy

--- Firmware Files/Marlin 1.1.9/Marlin/Configuration.h
+++ Firmware Files/Marlin 1.1.9/Marlin/Configuration.h
@@ -617,9 +617,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  2000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          1500    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   1500    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
@@ -629,10 +629,10 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 10.0
-#define DEFAULT_YJERK                 10.0
+#define DEFAULT_XJERK                 15.0
+#define DEFAULT_YJERK                 15.0
 #define DEFAULT_ZJERK                  0.3
-#define DEFAULT_EJERK                 10.0
+#define DEFAULT_EJERK                 15.0
 
 /**
  * S-Curve Acceleration
